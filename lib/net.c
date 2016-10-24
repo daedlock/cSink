9057324ecf05ed7c0797d73bc677a907bad85ba9
Hossam Saraya <hossam.saraya@gmail.com>
2016-10-24 16:45:47 +0200

    Stable

--- lib/net.c
+++ lib/net.c
@@ -1,19 +1,14 @@
 #include "net.h"
+#include <stdlib.h>
 
 
 
 /**
  * Used by CURL_WRITEFUNCTION to process response
  */
-size_t processResponse(void * ptr, size_t size, size_t nmemb, void * stream) {
-    printf("--[ processResponse called ]--\n");
-    printf("[*] ptr:\t\t%p\n", ptr);
-    printf("[*] size:\t\t%u\n", size);
-    printf("[*] nmemb:\t\t%u\n", nmemb);
-    cc_fprintf(CC_FG_GREEN, stdout, "--[ Completed Request ]--\n");
-    char **response_ptr =  (char**)stream;
-
-    *response_ptr = strndup(ptr, (size_t)(size *nmemb));
+size_t processResponse(void * ptr, size_t size, size_t nmemb, char *output) {
+    char * resPtr = (char *)ptr;
+    strcpy(output, resPtr);
 }
 
 /**
@@ -21,12 +16,12 @@ size_t processResponse(void * ptr, size_t size, size_t nmemb, void * stream) {
  */
 char * getUrl(char * url) {
     CURL *curl = curl_easy_init();
-    char * response = NULL;
+    char * response = (char *)malloc(9000);
     if(curl) {
         CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &processResponse);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
         return response;
